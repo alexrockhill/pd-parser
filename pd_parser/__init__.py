88bc4ee5421710c8d1d8c2c1e3a4a0fbe739ed99
Alex <aprockhill@mailbox.org>
2020-10-02 08:37:40 -0700

    moved to next dev version

--- pd_parser/__init__.py
+++ pd_parser/__init__.py
@@ -1,6 +1,6 @@
 """A toolbox for parsing potentially corrupted photodiode events."""
 
-__version__ = '0.2'
+__version__ = '0.3.dev'
 
 
 from pd_parser.parse_pd import (find_pd_params, parse_pd,  # noqa
