19f258d0cd2191c591dbdb542b32f6e4586e2c4f
Alex Rockhill <aprockhill@mailbox.org>
2022-09-19 13:43:15 -0700

    bump version for mne release

--- examples/plot_find_pd_on_and_off.py
+++ examples/plot_find_pd_on_and_off.py
@@ -45,11 +45,8 @@ raw, beh, events, corrupted_indices = \
 info = mne.create_info(['ch1', 'ch2', 'ch3'], raw.info['sfreq'],
                        ['seeg'] * 3)
 raw2 = mne.io.RawArray(np.random.random((3, raw.times.size)) * 1e-6, info)
-raw2.info['lowpass'] = raw.info['lowpass']  # these must match to combine
 raw.add_channels([raw2])
-# bids needs these data fields
-raw.info['dig'] = None
-raw.info['line_freq'] = 60
+raw.info['line_freq'] = 60  # for bids
 
 # add some offsets to the behavior so it's a bit more realistic
 offsets = np.random.randn(n_events) * 0.01
