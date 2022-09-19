73b2dcee44bdba0016912a687171c09ea8e29957
Alex Rockhill <aprockhill@mailbox.org>
2022-09-19 16:18:38 -0700

    fix version

--- pd_parser/__init__.py
+++ pd_parser/__init__.py
@@ -1,6 +1,6 @@
 """A toolbox for parsing potentially corrupted photodiode events."""
 
-__version__ = 'v0.9dev'
+__version__ = 'v0.9.dev0'
 
 
 from pd_parser.parse_pd import (find_pd_params, parse_pd, parse_audio, # noqa
